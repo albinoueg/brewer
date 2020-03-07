cd2dea48aae8695681bdc48cbe76f5145ee0a344
Albino Moreira <albino.sistemas@gmail.com>
2020-03-07 09:17:40 -0300

    Commit inicial

---
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -0,0 +1,19 @@
+package com.algaworks.brewer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class CervejasController {
+
+    @RequestMapping("/cervejas/novo")
+    public String novo(){
+        return "cerveja/CadastroCerveja";
+    }
+
+    @RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
+    public void cadastrar(){
+        //teste
+    }
+}
